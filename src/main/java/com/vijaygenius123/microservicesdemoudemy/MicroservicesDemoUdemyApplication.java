b526f31aea219658d99708d5f33e6a47c9b04a13
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-08-15 19:50:19 +0100

    Initial commit

---
+++ src/main/java/com/vijaygenius123/microservicesdemoudemy/MicroservicesDemoUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.microservicesdemoudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesDemoUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesDemoUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vijaygenius123/microservicesdemoudemy/MicroservicesDemoUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.microservicesdemoudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesDemoUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
